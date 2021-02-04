6da124f53bdcd130cebcf32bcf74d0c38b7af104
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-04 15:38:49 -0600

    GET MenuItems for Menu

--- api/menuItem.js
+++ api/menuItem.js
@@ -1,10 +1,20 @@
 // Create express Router
 const express = require('express');
-const timesheetRouter = express.Router({mergeParams: true});
+const menuItemRouter = express.Router({mergeParams: true});
 
 // Setup database
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menuItemRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM MenuItem WHERE menu_id = ${req.params.menuId}`, (error, menuItems) => {
+        if (error) {
+            next(error);
+        } else {
+            res.status(200).json({menuItems: menuItems});
+        }
+    });
+});
+
 // Export router
-module.exports = timesheetRouter;
+module.exports = menuItemRouter;

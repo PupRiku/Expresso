66a5ef91083d48425b9085f5d5d087fbada3fc1f
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-04 14:11:37 -0600

    GET all menus

--- api/menu.js
+++ api/menu.js
@@ -6,5 +6,15 @@ const menuRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menuRouter.get('/', (req, res, next) => {
+    db.all('SELECT * FROM Menu', (error, menus) => {
+        if (error) {
+            next(error);
+        } else {
+            res.status(200).json({menus: menus});
+        }
+    });
+});
+
 // Export router
 module.exports = menuRouter;

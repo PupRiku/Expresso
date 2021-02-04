5881fb858431d2090c23a200f42d5d29f93c56eb
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-04 12:07:01 -0600

    POST new timesheet for employee

--- api/timesheet.js
+++ api/timesheet.js
@@ -16,5 +16,34 @@ timesheetRouter.get('/', (req, res, next) => {
     });
 });
 
+timesheetRouter.post('/', (req, res, next) => {
+    const hours = req.body.timesheet.hours;
+    const rate = req.body.timesheet.rate;
+    const date = req.body.timesheet.date;
+    if(!hours || ! rate || !date ) {
+        return res.sendStatus(400);
+    }
+    const sql = 'INSERT INTO Timesheet (hours, rate, date, employee_id) VALUES ($hours, $rate, $date, $employeeId)';
+    const values = {
+        $hours: hours,
+        $rate: rate,
+        $date: date,
+        $employeeId: req.params.employeeId
+    };
+    db.run(sql, values, function(error) {
+        if (error) {
+            next(error);
+        } else {
+            db.get(`SELECT * FROM Timesheet WHERE id = ${this.lastID}`, (error, timesheet) => {
+                if (error) {
+                    next(error);
+                } else {
+                    res.status(201).json({timesheet: timesheet});
+                }
+            })
+        }
+    });
+});
+
 // Export router
 module.exports = timesheetRouter;

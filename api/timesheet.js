46cb902e902018f023aebe79c08dff6b44d3808e
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-04 11:53:22 -0600

    GET Timesheets for employee

--- api/timesheet.js
+++ api/timesheet.js
@@ -1,12 +1,20 @@
 // Create express Router
 const express = require('express');
-const timesheetRouter = express.Router();
+const timesheetRouter = express.Router({mergeParams: true});
 
 // Setup database
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
-
+timesheetRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM Timesheet WHERE employee_id = ${req.params.employeeId}`, (error, timesheets) => {
+        if (error) {
+            next(error);
+        } else {
+            res.status(200).json({timesheets: timesheets});
+        }
+    });
+});
 
 // Export router
 module.exports = timesheetRouter;

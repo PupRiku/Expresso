eeb4f807d86b1e4fe662b58f4b166c1f8ce761e4
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-04 11:19:31 -0600

    Timesheet skeleton

---
+++ api/timesheet.js
@@ -0,0 +1,12 @@
+// Create express Router
+const express = require('express');
+const timesheetRouter = express.Router();
+
+// Setup database
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+
+
+// Export router
+module.exports = timesheetRouter;

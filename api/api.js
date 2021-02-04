b772cbbf20b54dfa1227fd7f4bc7806a8dceaab7
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-02-04 09:38:47 -0600

    Setup main employee and menu routers

--- api/api.js
+++ api/api.js
@@ -4,5 +4,13 @@ const express = require('express');
 // setup api router
 const apiRouter = express.Router();
 
+// Import routers
+const employeeRouter = require('./employee');
+const menuRouter = require('./menu');
+
+// Mount routes
+apiRouter.use('/employees', employeeRouter);
+apiRouter.use('/menu', menuRouter);
+
 // Export router
 module.exports = apiRouter;
---
+++ api/employee.js
@@ -0,0 +1,10 @@
+// Create express Router
+const express = require('express');
+const employeeRouter = express.Router();
+
+// Setup database
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+// Export router
+module.exports = artistsRouter;
